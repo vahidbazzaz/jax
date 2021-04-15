Fix jax2tf fails to convert jax.random.randint due to unsigned types used with tf.abs and tf.sign.
PiperOrigin-RevId: 368617093

jax/experimental/jax2tf/jax2tf.py
@@ -887,8 +887,15 @@ def _div(lhs, rhs):
 
 
 def _rem(lhs, rhs):
-  return tf.math.sign(lhs) * tf.math.floormod(tf.math.abs(lhs),
-                                              tf.math.abs(rhs))
+  if not lhs.dtype.is_unsigned:
+    sign = tf.math.sign(lhs)
+    lhs = tf.math.abs(lhs)
+  else:
+    sign = tf.ones_like(lhs)
+  if not rhs.dtype.is_unsigned:
+    rhs = tf.math.abs(rhs)
+  return sign * tf.math.floormod(lhs, rhs)
+
 
 tf_impl[lax.div_p] = _div
 tf_impl[lax.rem_p] = _rem